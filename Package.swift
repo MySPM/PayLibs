a2ed4099e834199d5e82446ab3673749ca7fbe5c
dy <86118@163.com>
2023-10-07 19:01:51 +0800

    修正部分警告，升级CommonLibs=0.1.1

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "git@github.com:andforce/CommonLibs.git", .exact("0.0.7"))
+        .package(url: "git@github.com:andforce/CommonLibs.git", from: "0.1.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
